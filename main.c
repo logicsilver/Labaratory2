4a630d7ac09043f821aca612212d468c2fb61080
logicsilver <johnsilverrtx@gmail.com>
2026-09-09 18:35:04 +0500

    Добавьте файлы проекта.

---
+++ main.c
@@ -0,0 +1,100 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <Windows.h>
+
+#define menu_exit    0
+#define menu_task_1  1
+#define menu_task_2  2
+#define menu_task_3  3
+#define menu_task_4  4
+#define menu_task_5  5
+#define menu_task_6  6
+
+
+
+
+#define item_empty          0  //Пустота
+#define item_wood           1  //Дерево
+#define item_stone          2  //Камень
+#define item_seeds          3  //Семена
+#define item_iron           4  //Железо
+#define item_gold           5  //Золото
+#define item_hoe            6  //Мотыга
+#define item_fertilizer     7  //Удобрение
+#define item_water_can      8  //Лейка
+#define item_milk           9  //Молоко
+
+
+int main() {
+
+	SetConsoleCP(65001);
+	SetConsoleOutputCP(65001);
+
+	bool run = true;
+
+	int current_day = 1;
+	int current_hour = 8;
+	int inventory[10] = {0};
+
+
+	inventory[0] = item_hoe;
+	inventory[1] = item_water_can;
+	inventory[2] = item_seeds;
+	inventory[3] = item_wood;
+	inventory[4] = item_seeds;
+	inventory[5] = item_stone;
+	inventory[6] = item_hoe;
+	inventory[7] = item_seeds;
+	inventory[8] = item_seeds;
+	inventory[9] = item_stone;
+
+
+	while (run) {
+		int choice;
+
+		printf("\n====MAIN_MENU====\n");
+		printf("\n   [0] - Выход\n");
+		printf("\n   [1] - Посмотреть на часы\n");
+		printf("\n   [2] - Промотать время\n");
+		printf("\n   [3] - Просмотреть инвентарь\n");
+		printf("\n   [4] - Положить предмет в слот\n");
+		printf("\n   [5] - Выбросить предмет\n");
+		printf("\n   [6] - Устранение дубликатов\n");
+		printf("\nВведите пунт -> ");
+
+
+		while (scanf("%d", &choice) != 1) {
+			printf("Ошибка. Введено не число. Повторите ввод -> ");
+		};
+
+		switch (choice) {
+		case menu_task_1:
+			printf("\n[Часы фермера]\n");
+			printf("\nТекущее время: День %d, %02d:00\n",current_day, current_hour);
+				break;
+		case menu_task_2:
+			printf("\n[Выполняется задача номер 2]\n");
+			break;
+		case menu_task_3:
+			printf("\n[Выполняется задача номер 3]\n");
+			break;
+		case menu_task_4:
+			printf("\n[Выполняется задача номер 4]\n");
+			break;
+		case menu_task_5:
+			printf("\n[Выполняется задача номер 5]\n");
+			break;
+		case menu_task_6:
+			printf("\n[Выполняется задача номер 6\n");
+			break;
+		case menu_exit:
+			printf("\n[Выполняется  выход]\n");
+			break;
+
+		}
+
+	
+	}
+}
